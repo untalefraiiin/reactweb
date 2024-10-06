bffa4b9c7190eed0445900b79fe2902fa925cf06
UnTalEfraiiin <efrainsulbaran2001@gmail.com>
2024-10-06 13:56:51 -0400

    New changes from all

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
   const [nombre, setNombre] = useState();
   const [apellido, setApellido] = useState();
   const [email, setEmail] = useState();
+  const [id, setId] = useState(0);
+  const [editActive, setEditActive] = useState(false);
 
   const add = () =>{
     axios.post("http://localhost:3001/create", {
@@ -14,9 +16,45 @@ function App() {
       apellido: apellido,
       email: email
     }).then(()=>{
-      getRegistros();
       alert('Registro enviado exitosamente :D');
+      getRegistros();
     })
+  }
+
+  const update = () =>{
+    axios.post("http://localhost:3001/update", {
+      nombre: nombre,
+      apellido: apellido,
+      email: email, 
+      id
+    }).then(()=>{
+      alert('Editado enviado exitosamente :D');
+      getRegistros();
+    })
+  }
+
+  const deleteRegistro = (id) =>{
+    axios.put('http://localhost:3001/delete/'+id+'').then(()=>{
+      alert('Editado borrado exitosamente :c');
+      getRegistros();
+    })
+  }
+
+  const editRegistro = (value) =>{
+    setEditActive(true);
+    setNombre(value.nombre);
+    setApellido(value.apellido);
+    setEmail(value.email);
+    setId(value.id);
+  }
+
+  const cancelEditRegistro = () =>{
+    setEditActive(false);
+    
+    setNombre("");
+    setApellido("");
+    setEmail("");
+    setId(0);
   }
 
   const [registrosList, setRegistrosList] = useState([]);
@@ -29,7 +67,6 @@ function App() {
 
   getRegistros();
 
-
   return (
     <div className="App">
       <div className='container'>
@@ -41,34 +78,57 @@ function App() {
             <div className='card-body'>
               <div className='col-md-12 p-1 input-group'>
                 <label className='input-group-text'>Nombre</label>
-                <input id="name" onChange={((event)=>{setNombre(event.target.value)})} className='rounded form-control'></input>
+                <input id="name" onChange={((event)=>{setNombre(event.target.value)})} className='rounded form-control' value={nombre}></input>
               </div>
               <div className='col-md-12 p-1 input-group'>
                 <label className='input-group-text'>Apellido</label>
-                <input id="apellido" onChange={((event)=>{setApellido(event.target.value)})} className='rounded form-control'></input>
+                <input id="apellido" onChange={((event)=>{setApellido(event.target.value)})} className='rounded form-control' value={apellido}></input>
               </div>
               <div className='col-md-12 p-1 input-group'>
                 <label className='input-group-text'>Correo</label>
-                <input id="email" onChange={((event)=>{setEmail(event.target.value)})} className='rounded form-control'></input>
+                <input id="email" onChange={((event)=>{setEmail(event.target.value)})} className='rounded form-control' value={email}></input>
               </div>
               <div className='col-md-12 p-1 mt-2'>
-                <button onClick={add} className='btn btn-success col-md-12'>Enviar</button>
+                {
+                  editActive ?
+                  <div>
+                    <button onClick={()=>{update()}} className='btn btn-warning col-md-12 m-2'>Actualizar</button>
+                    <button onClick={()=>{cancelEditRegistro()}} className='btn btn-danger col-md-12 m-2'>Cancelar</button>
+                  </div>
+                  :
+                  <button onClick={add} className='btn btn-success col-md-12'>Enviar</button>
+                }
               </div>
+              <input id='id' type="hidden" value={id}></input>
             </div>
           </section>
         </div>
       </div>
 
-      <div>
-        <div className='col-md-12 p-3 m-2'>
-          <button className='btn btn-primary col-md-12 p-2' onClick={getRegistros}>Alistar</button>
-        </div>
-        <div>
-          {
-            registrosList.map((val,key)=>{
-              return <div className='col-md-12'>{val.nombre} - {val.apellido}</div>
-            })
-          }
+      <div className='container'>
+        <div className='row'>
+          
+          <table className='table table-striped table-light'>
+            <thead className="thead-light">
+              <tr>
+                <th scope="col">Nombre</th>
+                <th scope="col">Apellido</th>
+                <th scope="col">Correo</th>
+                <th scope="col">Editar</th>
+                <th scope="col">Borrar</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                registrosList.map((val,key)=>{
+                  return <tr className='col-md-12' scope="row"> <td>{val.nombre}</td> <td>{val.apellido}</td> <td>{val.email}</td> <td><button className='btn btn-success' onClick={()=>{editRegistro(val)}}>Editar</button></td> <td><button className='btn btn-danger' onClick={()=>{deleteRegistro(val.id)}}>Eliminar</button></td></tr>
+                })
+              }
+            </tbody>
+          </table>
+          <div>
+            
+          </div>
         </div>
       </div>
     </div>
